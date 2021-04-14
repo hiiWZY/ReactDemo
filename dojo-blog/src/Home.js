84118df1661fa165c2f2b0d706cf087578e61321
Wang.Zhenyu <Wang.Zhenyu@bizerba.com>
2021-04-14 13:30:01 +0800

    first commit

---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
+export default Home;
